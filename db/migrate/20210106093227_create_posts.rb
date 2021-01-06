c8cbf37cd48fa6ee8d34cbc3fccf2c4e22a79ace
yassunsun <ww16471z@icloud.com>
2021-01-06 18:49:21 +0900

    postsテーブルの作成

---
+++ db/migrate/20210106093227_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :name,    null: false
+      t.text   :content, null: false
+
+      t.timestamps
+    end
+  end
+end
