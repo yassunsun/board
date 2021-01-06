166ef72ccf3e5443e02d0218ce35b272fb81a076
yassunsun <ww16471z@icloud.com>
2021-01-06 19:24:27 +0900

    ストロングパラメーター

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def create
+  end
+
+  private
+ 
+  def post_params
+    params.require(:post).permit(:name, :content)
   end
 end
