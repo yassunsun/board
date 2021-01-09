41fc8f6bab9ebb654b0873cc7f83ebe9e98d8a1f
yassunsun <ww16471z@icloud.com>
2021-01-09 16:59:14 +0900

    newファイルの作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,10 @@
 class TopicsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'topics#index'
+  resources :topics, only: [:index, :new, :create]
   resources :posts, only: [:index, :create]
 end
