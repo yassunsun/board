6a098d10e4adf2776908e965f3bb8737ae2541bb
yassunsun <ww16471z@icloud.com>
2021-01-09 17:24:45 +0900

    新規スレッドの保存

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,24 @@
 class TopicsController < ApplicationController
   def index
+    @topics = Topic.order('created_at DESC')
   end
 
   def new
+    @topic = Topic.new
   end
 
   def create
+    @topic = Topic.create(topic_params)
+    if @topic.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+ 
+  def topic_params
+    params.require(:topic).permit(:title)
   end
 end
