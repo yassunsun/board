a8a0972f1b4946bc4873155c6c7460f89efbc878
yassunsun <ww16471z@icloud.com>
2021-01-10 19:20:54 +0900

    postsテーブルの作成

--- db/migrate/20210110101833_create_posts.rb
+++ db/migrate/20210110101833_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
+      t.text :content, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_101545) do
+ActiveRecord::Schema.define(version: 2021_01_10_101833) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
