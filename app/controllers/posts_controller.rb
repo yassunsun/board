b5e66c76ba980c4bbf77375441d8f4d6208df75a
yassunsun <ww16471z@icloud.com>
2021-01-10 23:35:55 +0900

    topicsとpostsの紐付け

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
     @topic = Topic.find(params[:topic_id])
+    @posts = @topic.posts
   end
 
   def create
-    @post = Post.new(post_params)
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.new(post_params)
     if @post.save
-      redirect_to topic_posts_path
+      redirect_to topic_posts_path(@topic)
     # else
     #   render :index
     end
