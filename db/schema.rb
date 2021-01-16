c2dd71dc6aef6fb4c8411dccf5d6e9b42a8b8779
yassunsun <ww16471z@icloud.com>
2021-01-16 23:53:41 +0900

    postsテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_140944) do
-
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "comment", null: false
-    t.bigint "topic_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["topic_id"], name: "index_posts_on_topic_id"
-  end
+ActiveRecord::Schema.define(version: 2021_01_16_145203) do
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -39,5 +31,4 @@ ActiveRecord::Schema.define(version: 2021_01_16_140944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "topics"
 end
