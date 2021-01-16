6b57c77488d86fa102347c2fa110ef703d127875
yassunsun <ww16471z@icloud.com>
2021-01-16 21:20:16 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+  }
   root to: 'topics#index'
   resources :topics, only: [:index, :new, :create] do
     resources :posts, only: [:index, :create]
