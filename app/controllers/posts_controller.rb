361e951722c0a77a036012211d53c1b1a47edb26
yassunsun <ww16471z@icloud.com>
2021-01-17 02:08:51 +0900

    コントローラーの編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @post = Post.new
     @topic = Topic.find(params[:topic_id])
-    @posts = @topic.posts
+    @posts = @topic.posts.includes(:user)
   end
 
   def create
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     if @post.save
       render json:{ post: @post }
     else
-      @posts = @topic.posts
+      @posts = @topic.posts.includes(:user)
       render :index
     end
   end
@@ -19,6 +19,6 @@ class PostsController < ApplicationController
   private
  
   def post_params
-    params.require(:post).permit(:comment)
+    params.require(:post).permit(:comment).merge(user_id: current_user.id)
   end
 end
