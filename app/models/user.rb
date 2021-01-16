8eac92dfbb4705610bb7fb9fcc82586fc3c06c7f
yassunsun <ww16471z@icloud.com>
2021-01-16 23:12:55 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,5 @@ class User < ApplicationRecord
   has_many :sns_credentials, dependent: :destroy
 
   validates :nickname, presence: true, length: { maximum: 6 }
-
-  def self.from_omniauth(auth)
-    sns = SnsUser.where(provider: auth.provider, uid: auth.uid).first_or_create
-    user = User.where(email: auth.info.email).first_or_initialize(
-       nickname: auth.info.name,
-       email: auth.info.email
-     )
- 
-   # userが登録済みであるか判断
-    if user.persisted?
-      sns.user = user
-      sns.save
-    end
-    user
-  end
+  
 end
