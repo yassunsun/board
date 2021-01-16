56847ee716c93c6747407cbfac500f0fe9ed32c0
yassunsun <ww16471z@icloud.com>
2021-01-16 21:02:03 +0900

    アソシエーションの設定

--- app/models/sns_user.rb
+++ app/models/sns_user.rb
@@ -1,2 +1,3 @@
 class SnsUser < ApplicationRecord
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  has_many :sns_credentials, dependent: :destroy
 
   validates :nickname, presence: true, length: { maximum: 6 }
 end
