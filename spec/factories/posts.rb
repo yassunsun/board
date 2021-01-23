ee7e30b3d9942c4f77db7cb9d6cf6342b8b3ad43
yassunsun <ww16471z@icloud.com>
2021-01-23 12:33:57 +0900

    postモデルのテスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :post do
-    
+    comment {Faker::Lorem.sentence}
+    association :user
+    association :topic
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe 'ポストの保存' do
+    context "ポストが保存できる場合" do
+      it "コメントがあればポストは保存される" do
+        expect(@post).to be_valid
+      end
+    end
+    context "ポストが保存できない場合" do
+      it "コメントがないとポストは保存できない" do
+        @post.comment = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Comment can't be blank")
+      end
+    end
+  end
 end
