ef3462598d54cfddc14ed563adbcc4db723dd06a
yassunsun <ww16471z@icloud.com>
2021-01-31 01:40:46 +0900

    userモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,54 +8,10 @@ class User < ApplicationRecord
   has_many :topic_users
   has_many :topics, through: :topic_users
   has_many :posts
+  has_many :sns_credentials
 
   validates :nickname, presence: true
 
-  # def self.without_sns_data(auth)
-  #   user = User.where(email: auth.info.email).first
-
-  #     if user.present?
-  #       sns = SnsCredential.create(
-  #         uid: auth.uid,
-  #         provider: auth.provider,
-  #         user_id: user.id
-  #       )
-  #     else
-  #       user = User.new(
-  #         nickname: auth.info.name,
-  #         email: auth.info.email,
-  #       )
-  #       sns = SnsCredential.new(
-  #         uid: auth.uid,
-  #         provider: auth.provider
-  #       )
-  #     end
-  #     return { user: user ,sns: sns}
-  #   end
-
-  #  def self.with_sns_data(auth, snscredential)
-  #   user = User.where(id: snscredential.user_id).first
-  #   unless user.present?
-  #     user = User.new(
-  #       nickname: auth.info.name,
-  #       email: auth.info.email,
-  #     )
-  #   end
-  #   return {user: user}
-  #  end
-
-  #  def self.find_oauth(auth)
-  #   uid = auth.uid
-  #   provider = auth.provider
-  #   snscredential = SnsCredential.where(uid: uid, provider: provider).first
-  #   if snscredential.present?
-  #     user = with_sns_data(auth, snscredential)[:user]
-  #     sns = snscredential
-  #   else
-  #     user = without_sns_data(auth)[:user]
-  #     sns = without_sns_data(auth)[:sns]
-  #   end
-  #   return { user: user ,sns: sns}
-  # end
-  
+  def self.from_omniauth(auth)
+  end
 end
