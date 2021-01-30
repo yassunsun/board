cf13ab938d1960ca94712d8c47fd4d2260672ab2
yassunsun <ww16471z@icloud.com>
2021-01-31 01:44:43 +0900

    userモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,10 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
   end
 end
