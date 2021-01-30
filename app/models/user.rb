ff3a8367c8b53082761d8d951e7fe8ad79107d8d
yassunsun <ww16471z@icloud.com>
2021-01-31 01:52:28 +0900

    userモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,10 @@ class User < ApplicationRecord
       nickname: auth.info.name,
         email: auth.info.email
     )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
   end
 end
